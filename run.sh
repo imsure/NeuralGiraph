1cc90d0d3f8c55361a3d892c9c0e184281422f62
Shuo Yang <imsure95@gmail.com>
2014-07-11 14:49:32 -0500

    modify scripts

--- run.sh
+++ run.sh
@@ -4,5 +4,5 @@
 
 hadoop fs -rm -r neuron_output
 
-hadoop jar giraph_neuron_graph-1.0.jar org.apache.giraph.GiraphRunner -Dgiraph.zkList=m02.gps.stthomas.edu:2181 -Dgiraph.doOutputDuringComputation=true -Dmax_supersteps=500 edu.stthomas.gps.NeuronVertex -vif edu.stthomas.gps.NeuronVertexTextInputFormat -vip neuron_input -of edu.stthomas.gps.NeuronVertexTextOutputFormat -op neuron_output -w 10
+hadoop jar giraph_neuron_graph-1.0.jar org.apache.giraph.GiraphRunner -Dgiraph.zkList=m02.gps.stthomas.edu:2181 -Dgiraph.doOutputDuringComputation=true -Dmax_supersteps=500 edu.stthomas.gps.NeuronVertex -vif edu.stthomas.gps.NeuronVertexTextInputFormat -vip neuron_input -of edu.stthomas.gps.NeuronVertexTextOutputFormat -op neuron_output -w 10 
 
